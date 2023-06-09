8509178641b617cc566bfa8c5c862eac50dbfa73
Hrishita-Paul <hrishita.paul03@gmail.com>
2023-06-10 00:13:34 +0530

    fix:book issue row

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-import React, { useState } from "react";
-import { Navbar, Nav } from "react-bootstrap";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-import HomePage from "./Pages/HomePage";
-import StudentDetails from "./Pages/StudentDetails";
-import BookDetails from "./Pages/BookDetails";
-import BookIssue from "./Pages/BookIssue";
+import HomePage from './Pages/HomePage';
+import StudentDetails from './Pages/StudentDetails';
+import BookIssue from './Pages/BookIssue';
 
 export default function App() {
   // Define state to hold the book details as an array
   const [bookDetails, setBookDetails] = useState([]);
 
   // Function to add a new book to the book details
-  const addBook = (newBook) => {
-    setBookDetails([...bookDetails, newBook]);
+  const onBookIssueSubmit = (newBookIssue) => {
+    setBookDetails((prevBookDetails) => [...prevBookDetails, newBookIssue]);
   };
-  return (
 
+  return (
     <Router>
       <Navbar bg="dark" expand="lg" variant="dark" className="fixed-top">
-        <Navbar.Brand href="/">Home</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          Home
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/studentDetails" style={{ color: "white" }}>
+            <Nav.Link as={Link} to="/studentDetails" style={{ color: 'white' }}>
               Student Details
             </Nav.Link>
-            <Nav.Link href="/bookDetails" style={{ color: "white" }}>
-              Book Details
-            </Nav.Link>
-            <Nav.Link href="/bookIssue" style={{ color: "white" }}>
+            <Nav.Link as={Link} to="/bookIssue" style={{ color: 'white' }}>
               Book Issue
             </Nav.Link>
           </Nav>
@@ -37,16 +35,11 @@ export default function App() {
       </Navbar>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/studentDetails" element={<StudentDetails/>} />
-        <Route
-          exact
-          path="/bookDetails"
-          element={<BookDetails bookDetails={bookDetails} />}
-        />
+        <Route exact path="/studentDetails" element={<StudentDetails />} />
         <Route
           exact
           path="/bookIssue"
-          element={<BookIssue onBookIssueSubmit={addBook} />}
+          element={<BookIssue bookDetails={bookDetails} onBookIssueSubmit={onBookIssueSubmit} />}
         />
       </Routes>
     </Router>
--- src/Pages/BookIssue.jsx
+++ src/Pages/BookIssue.jsx
@@ -1,62 +1,118 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+
 import './BookIssue.css';
 
-class BookIssue extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      studentName: '',
-      bookName: '',
-      dateOfIssue: '',
-      lastDateOfReturn: '',
-    };
-  }
+const BookIssue = ({ onBookIssueSubmit }) => {
+  const [studentName, setStudentName] = useState('');
+  const [bookName, setBookName] = useState('');
+  const [dateOfIssue, setDateOfIssue] = useState('');
+  const [lastDateOfReturn, setLastDateOfReturn] = useState('');
+  const [filteredBookDetails, setFilteredBookDetails] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
-  handleInputChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
+  useEffect(() => {
+    const storedBookDetails = localStorage.getItem('bookDetails');
+    if (storedBookDetails) {
+      setFilteredBookDetails(JSON.parse(storedBookDetails));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('bookDetails', JSON.stringify(filteredBookDetails));
+  }, [filteredBookDetails]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'studentName') {
+      setStudentName(value);
+    } else if (name === 'bookName') {
+      setBookName(value);
+    } else if (name === 'dateOfIssue') {
+      setDateOfIssue(value);
+    } else if (name === 'lastDateOfReturn') {
+      setLastDateOfReturn(value);
+    }
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const { studentName, bookName, dateOfIssue, lastDateOfReturn } = this.state;
-  
-    // Create a new book issue object
+
     const newBookIssue = {
       studentName,
       bookName,
       dateOfIssue,
       lastDateOfReturn,
     };
-  
-    // Call the function passed from the parent component to handle book issue submission
-    this.props.onBookIssueSubmit(newBookIssue);
-  
-    // Clear the form fields
-    this.setState({
-      studentName: "",
-      bookName: "",
-      dateOfIssue: "",
-      lastDateOfReturn: "",
-    });
+
+    onBookIssueSubmit(newBookIssue);
+
+    setFilteredBookDetails((prevFilteredDetails) => [...prevFilteredDetails, newBookIssue]);
+
+    setStudentName('');
+    setBookName('');
+    setDateOfIssue('');
+    setLastDateOfReturn('');
   };
-  
 
-  render() {
-    const { studentName, bookName, dateOfIssue, lastDateOfReturn } = this.state;
+  const handleSearch = (e) => {
+    const searchValue = e.target.value;
+    setSearchValue(searchValue);
+    const filteredDetails = filteredBookDetails.filter((bookIssue) =>
+      bookIssue.studentName.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setFilteredBookDetails(filteredDetails);
+  };
 
-    return (
-      <div className="page-content">
-        <div className="container my-3">
-          <p className="heading">Book Issue</p>
-          <hr />
+  const renderTableRows = () => {
+    if (searchValue) {
+      return filteredBookDetails.map((bookIssue, index) => (
+        <tr key={index}>
+          <td>{bookIssue.studentName}</td>
+          <td>{bookIssue.bookName}</td>
+          <td>{bookIssue.dateOfIssue}</td>
+          <td>{bookIssue.lastDateOfReturn}</td>
+        </tr>
+      ));
+    } else {
+      return filteredBookDetails.map((bookIssue, index) => (
+        <tr key={index}>
+          <td>{bookIssue.studentName}</td>
+          <td>{bookIssue.bookName}</td>
+          <td>{bookIssue.dateOfIssue}</td>
+          <td>{bookIssue.lastDateOfReturn}</td>
+        </tr>
+      ));
+    }
+  };
 
-          {/* Form section */}
-          <form onSubmit={this.handleSubmit}>
-            {/* Student Name input */}
+  const handleClearAll = () => {
+    localStorage.removeItem('bookDetails');
+    setFilteredBookDetails([]);
+  };
+
+  return (
+    <div className="page-content">
+      <div className="container my-3">
+        <p className="heading">Book Issue</p>
+        <hr />
+
+        <div>
+          <div className="form-group row">
+            <div className="col-sm-12 my-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search by student name"
+                onChange={handleSearch}
+              />
+            </div>
+          </div>
+
+          <form onSubmit={handleSubmit}>
             <div className="form-group row">
-              <label htmlFor="studentName" className="label-text">Student Name</label>
+              <label htmlFor="studentName" className="label-text">
+                Student Name
+              </label>
               <div className="col-sm-12">
                 <input
                   type="text"
@@ -65,15 +121,16 @@ class BookIssue extends Component {
                   name="studentName"
                   placeholder="Student Name"
                   value={studentName}
-                  onChange={this.handleInputChange}
+                  onChange={handleInputChange}
                 />
               </div>
             </div>
             <br />
 
-            {/* Book Name input */}
             <div className="form-group row">
-              <label htmlFor="bookName" className="label-text">Name of Book</label>
+              <label htmlFor="bookName" className="label-text">
+                Name of Book
+              </label>
               <div className="col-sm-12">
                 <input
                   type="text"
@@ -82,15 +139,16 @@ class BookIssue extends Component {
                   name="bookName"
                   placeholder="Book Name"
                   value={bookName}
-                  onChange={this.handleInputChange}
+                  onChange={handleInputChange}
                 />
               </div>
             </div>
             <br />
 
-            {/* Date of Issue input */}
             <div className="form-group row">
-              <label htmlFor="dateOfIssue" className="label-text">Date of Issue</label>
+              <label htmlFor="dateOfIssue" className="label-text">
+                Date of Issue
+              </label>
               <div className="col-sm-12">
                 <input
                   type="date"
@@ -99,15 +157,16 @@ class BookIssue extends Component {
                   name="dateOfIssue"
                   placeholder="Date of Issue"
                   value={dateOfIssue}
-                  onChange={this.handleInputChange}
+                  onChange={handleInputChange}
                 />
               </div>
             </div>
             <br />
 
-            {/* Last Date of Return input */}
             <div className="form-group row">
-              <label htmlFor="lastDateOfReturn" className="label-text">Last Date of Return</label>
+              <label htmlFor="lastDateOfReturn" className="label-text">
+                Last Date of Return
+              </label>
               <div className="col-sm-12">
                 <input
                   type="date"
@@ -116,13 +175,12 @@ class BookIssue extends Component {
                   name="lastDateOfReturn"
                   placeholder="Last Date of Return"
                   value={lastDateOfReturn}
-                  onChange={this.handleInputChange}
+                  onChange={handleInputChange}
                 />
               </div>
             </div>
             <br />
 
-            {/* Submit button */}
             <div className="form-group row">
               <div className="col-sm-10">
                 <button type="submit" className="btn btn-primary">
@@ -131,10 +189,28 @@ class BookIssue extends Component {
               </div>
             </div>
           </form>
+
+          <div>
+            <p className="heading">Book Details</p>
+            <table className="table table-striped">
+              <thead>
+                <tr className="table-warning">
+                  <th scope="col">Student Name</th>
+                  <th scope="col">Book Name</th>
+                  <th scope="col">Date of Issue</th>
+                  <th scope="col">Last Date of Return</th>
+                </tr>
+              </thead>
+              <tbody>{renderTableRows()}</tbody>
+            </table>
+            <button className="btn btn-danger" onClick={handleClearAll}>
+              Clear All
+            </button>
+          </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default BookIssue;
